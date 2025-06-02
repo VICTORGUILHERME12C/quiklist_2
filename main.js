8dac17c2dd0e257873c69f0295da96cbb629fe6a
VICTORGUILHERME12C <164795573+VICTORGUILHERME12C@users.noreply.github.com>
2025-06-01 22:01:44 -0300

    adiconando img ao js

--- main.js
+++ main.js
@@ -18,3 +18,20 @@ document.querySelector("#item").addEventListener("keydown", (event) => {
 function showItemsList() {
   const sectionList = document.querySelector(".list")
   sectionList.textContent = ""
+  items.sort((itemA, itemB) => Number(itemA.checked) - Number(itemB.checked))
+items.map((item, index) => {
+  sectionList.innerHTML += `
+    <div class="item">
+      <div>
+        <input type="checkbox" name="list" id="item-${index}" ${item.checked && "checked"}>
+        <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+          <img src="./assets/checked.svg" alt="checked">
+        </div>
+        <label for="item-${index}" onclick="checkItem('${item.name}')">${item.name}</label>
+      </div>
+      <button onclick="removeItem('${item.name}')">
+        <img src="./assets/trash-icon.svg" alt="trash-icon">
+      </button>
+    </div>
+  `
+})
