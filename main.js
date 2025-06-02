b7507a9e2642a72ebd7987cd741d50bd355556fb
VICTORGUILHERME12C <164795573+VICTORGUILHERME12C@users.noreply.github.com>
2025-06-01 22:05:02 -0300

    setimo commit js

--- main.js
+++ main.js
@@ -37,3 +37,15 @@ items.map((item, index) => {
 })
 localStorage.setItem("items", JSON.stringify(items))
 }
+function removeItem(itemName) {
+  const itemIndex = items.findIndex((item) => item.name === itemName)
+  const divWarning = document.querySelector(".warning")
+  divWarning.classList.remove("hide-warning")
+  setTimeout(() => {
+    divWarning.classList.add("hide-warning")
+  }, 4000)
+  if (itemIndex !== -1) {
+    items.splice(itemIndex, 1)
+  }
+  showItemsList()
+}
